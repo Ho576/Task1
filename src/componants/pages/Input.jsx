b328c88cbecb4eef6128be7e1c6f0cc87eecb05d
Ho576 <127043686+Ho576@users.noreply.github.com>
2023-11-23 08:18:42 +0200

    Update Input.jsx

--- src/componants/pages/Input.jsx
+++ src/componants/pages/Input.jsx
@@ -9,19 +9,19 @@ export default function Input({type='text',id,name,title,value,onChange,errors,o
     //console.log(touched)
   return (
     <>
-    <div className="input-group mb-3">
+    {/* <div className="input-group mb-3">
         <label htmlFor={id} >{title}</label><br/>
         <input type={type} name={name} className="form-control" value={value} id={id} onChange={onChange} onBlur={onBlur} />
-        {touched[name]&&errors[name]&&<p className='text text-danger'>{errors[name]}</p>}</div>
+        {touched[name]&&errors[name]&&<p className='text text-danger'>{errors[name]}</p>}</div>*/}
 
-        {/* <div className="d-flex flex-row align-items-center mb-4">
+        <div className="d-flex flex-row align-items-center mb-4">
                     <i className="fas fa-envelope fa-lg me-3 fa-fw" />
                     <div className="form-outline flex-fill mb-0">
-                      <input type={type} name={name} className="form-control" value={value} id={id} onChange={onChange} onClick={handleInputClick} />
+                      <input type={type} name={name} className="form-control" value={value} id={id} onChange={onChange} onBlur={onBlur} />
                       <label className="form-label"  htmlFor={id}>{title}</label>
                       {touched[name]&&errors[name]&&<p className='text text-danger'>{errors[name]}</p>} 
                     </div>
-                  </div> */}
+                  </div> 
         
     
     </>
